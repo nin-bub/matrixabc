7ebae6882a9a4b9230cefe32436bfd8e94c25254
nin-bub <ninafrancesca.estolano@gmail.com>
2026-04-29 15:30:16 +0800

    Update tb.v

--- test/tb.v
+++ test/tb.v
@@ -28,7 +28,7 @@ module tb ();
 `endif
 
   // Replace tt_um_example with your module name:
-  tt_um_example sumo (
+  tt_um_vga_glyph_mode sumo (
 
       // Include power ports for the Gate Level test:
 `ifdef GL_TEST
